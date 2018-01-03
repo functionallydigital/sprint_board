97789cf8cadf5789d626217423c26fdc74ad02f0
Matt Nicks <matt.nicks@methods.co.uk>
2018-01-03 11:18:32 +0000

    don't return nil for input fields

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -2,7 +2,7 @@ class Priority
   PRIORITY_LEVELS = [{value: 1, label: 'Must'}, {value: 2, label: 'Should'}, {value: 3, label: 'Could'}, {value: 4, label: 'Would'}]
 
   def self.find_priority(id)
-    id ? PRIORITY_LEVELS.select{|level| level[:value] == id}.first : nil
+    id ? PRIORITY_LEVELS.select{|level| level[:value] == id}.first : ''
   end
 
   def self.find_label(id)
